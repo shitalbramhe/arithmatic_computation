730ebe8ac43336e27e4246d6a17325f62c42a885
shitalbramhe <shitalbramhe@gmail.com>
2021-10-14 12:34:36 +0530

    3rd eqn

--- arithmatic_computation.sh
+++ arithmatic_computation.sh
@@ -5,5 +5,8 @@ read a b c
 equ1=$(( $a + $b * $c ))
 echo "first equation" $equ1;
 
-qu2=$(( $a * $b + $c ))
+equ2=$(( $a * $b + $c ))
 echo "second equation" $equ2;
+
+equ3=$(( $c + $a / $b ))
+echo "third equation" $equ3;
