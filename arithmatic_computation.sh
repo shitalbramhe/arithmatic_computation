3553bdc8de79db9ee296c2298a25b2cce71a33e0
shitalbramhe <shitalbramhe@gmail.com>
2021-10-14 12:59:38 +0530

    dict value to array

--- arithmatic_computation.sh
+++ arithmatic_computation.sh
@@ -13,3 +13,27 @@ echo "third equation" $equ3;
 
 equ4=$(( $a % $b + $c ))
 echo "fourth equation" $equ4;
+
+declare -A  dict
+
+dict[1]=$equ1
+dict[2]=$equ2
+dict[3]=$equ3
+dict[4]=$equ4
+
+j=0
+
+echo "Printing Dictonary values";
+
+for(( j=1; j<=4; j++ ))
+do
+echo "Dict[$j]:"${dict[$j]}
+done
+
+i=1
+echo "Printing Array Values";
+for(( i=1; i<=4; i++ ))
+do
+arr1[i]=${dict[$i]}
+echo "Array[$i]:"${arr1[i]}
+done
